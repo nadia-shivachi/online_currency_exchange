07dfb2ec9a77c7448e441dace55191e0a39594c7
Nadia Shivachi <nadia.shivachi@gmail.com>
2022-08-23 14:55:42 +0200

    "resultElement" declared inside "Init"

--- script/script.js
+++ script/script.js
@@ -1,24 +1,22 @@
 {
-  const convertCurrency = () => {
+  const convertCurrency = (resultElement) => {
     const formElement = document.querySelector(".js-form");
     const plnElement = document.querySelector(".js-fieldPLN");
     const currencyElement = document.querySelector(".js-currency");
-    const resultElement = document.querySelector(".js-result");
 
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
 
-      let amount = plnElement.value;
-      let currency = currencyElement.value;
+      const amount = plnElement.value;
+      const currency = currencyElement.value;
       let result = amount / currency;
 
       resultElement.innerText = result.toFixed(2);
     });
   };
 
-  const resetResult = () => {
+  const resetResult = (resultElement) => {
     const resetElement = document.querySelector(".js-reset");
-    const resultElement = document.querySelector(".js-result");
 
     resetElement.addEventListener("click", () => {
       resultElement.innerText = "N/A";
@@ -26,8 +24,9 @@
   };
 
   const init = () => {
-    convertCurrency();
-    resetResult();
+    const resultElement = document.querySelector(".js-result");
+    convertCurrency(resultElement);
+    resetResult(resultElement);
   };
 
   init();
